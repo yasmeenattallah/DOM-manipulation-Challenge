0948607a5a43aff283efb8110014fa3684b23d09
yasmeenattallah <yattallah7@gmail.com>
2023-06-14 18:08:32 +0300

    challenge

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # DOM-manipulation-Challenge
+
+## Exercise
+
+1. Clone this repo.
+
+2. Open exercise.js and follow the instructions.
+
+## What is the DOM?
+
+The DOM is the _Document Object Model_, it is the browser's interpretation of the page once it has been parsed.
+It's something implemented by web browsers rather than provided by the JavaScript language itself.
+
+## DOM elements
+
+In your browser, if you _right-click_ on a page, you see _inspect element_.
+
+![inpect element](./assest/inspect.png)
---
+++ exercise.js
@@ -0,0 +1,39 @@
+// 1 - Write a function which, if the 'h2' element  has the class "header",
+// it will be removed; otherwise the class "toogleHeader" is added.
+
+const toggleClass = () => {};
+// toggleClass(); //uncomment me to test
+
+// 2- Select the section with an id of 'container' without using querySelector,
+// and style the font with 'blue' color
+
+const styleContainer = () => {};
+// styleContainer(); //uncomment me to test
+
+// 3 - Select the 'nav' with a class of "firstNav" and style the font with 'italic' fontStyle.
+
+const styleItalic = () => {};
+// styleItalic(); //uncomment me to test
+
+// 4 - Select a list item with a class of 'contact', but only the list item inside of the 'secondList' nav,
+// and style it with background color 'yellow'
+
+const backgroundList = () => {};
+
+// backgroundList(); //uncomment me to test
+
+// 5- Create a new 'li' element , give it the text 'project' and append it to the 'fisrtList' .
+const newList = () => {};
+newList(); //uncomment me to test
+
+// 6 - Loop over all of the 'li' elements inside the 'secondList' and give them a background color of "brown".
+const brownColor = () => {};
+// brownColor(); //uncomment me to test
+
+// 7- Create 'p' eleement with any text and append it to the footer
+const addFooter = () => {};
+// addFooter(); //uncomment me to test
+
+// 8- Remove the 'div' with a class of 'footer'.
+const removeFooter = () => {};
+// removeFooter(); //uncomment me to test
